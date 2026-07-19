9dcc815eb89dc14cb7133d311f53689328dbb87c
lgcarrilloc <mgoluiswar@hotmail.com>
2026-07-18 21:25:06 -0500

    Paso 11 integracion completa de la aplicacion

--- desarrollo web fronted/catalogo-jugadores/src/App.jsx
+++ desarrollo web fronted/catalogo-jugadores/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import JugadorList from "./components/JugadorList";
+import Footer from "./components/Footer";
 
 function App() {
   const [totalJugadores, setTotalJugadores] = useState(0);
@@ -11,6 +12,7 @@ function App() {
       <Navbar totalJugadores={totalJugadores} />
       <Header />
       <JugadorList onCargados={setTotalJugadores} />
+      <Footer />
     </>
   );
 }
---
+++ desarrollo web fronted/catalogo-jugadores/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Catálogo de Jugadores - React + Vite</p>
+      <p>Luis Gustavo Carrillo Chávez</p>
+    </footer>
+  );
+}
+
+export default Footer;
